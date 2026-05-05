42c1e5171e76bc9f1f0bab53aba8e6eb43d7ffe3
Kaled Al <kalalwayly@gmail.com>
2026-05-05 12:11:24 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,30 @@
+// Import Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-database.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-storage.js"; // Import Firebase Storage
+
+// Firebase config (replace with your Firebase credentials)
+const firebaseConfig = {
+  apiKey: "AIzaSyDBT8jv057_JQL6pIUYk-U_LQ8uJHlFi-o",
+  authDomain: "kal-marketplace.firebaseapp.com",
+  databaseURL: "https://kal-marketplace-default-rtdb.firebaseio.com",
+  projectId: "kal-marketplace",
+  storageBucket: "kal-marketplace.firebasestorage.app",
+  messagingSenderId: "745728416819",
+  appId: "1:745728416819:web:da2dfb86cc5b79fb0d1746",
+  measurementId: "G-FFHYQC4YJV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Auth, Firestore, Realtime Database, and Storage
+const auth = getAuth(app);
+const db = getFirestore(app);
+const rtdb = getDatabase(app);  // Realtime Database
+const storage = getStorage(app); // Initialize Firebase Storage
+
+// Export services for use in other files
+export { auth, db, rtdb, storage };  
