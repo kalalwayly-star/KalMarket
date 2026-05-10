1eb7a2c07870ef6548c37b4213e1577cea564e45
Kaled Al <kalalwayly@gmail.com>
2026-05-10 18:26:51 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -8,10 +8,10 @@ import { initializeFirestore } from "https://www.gstatic.com/firebasejs/12.12.1/
 // Firebase config (replace with your Firebase credentials)
 const firebaseConfig = {
   apiKey: "AIzaSyDBT8jv057_JQL6pIUYk-U_LQ8uJHlFi-o",
-  authDomain: "kal-marketplace.firebaseapp.com",
-  databaseURL: "https://kal-marketplace-default-rtdb.firebaseio.com",
-  projectId: "kal-marketplace", // Fix: Project ID usually doesn't end in .app
-  storageBucket: "kal-marketplace.appspot.com", // Fix: No spaces allowed here
+  authDomain: "://firebaseapp.com",
+  databaseURL: "https://firebaseio.com",
+  projectId: "kal-marketplace", 
+  storageBucket: "kal-marketplace.firebasestorage.app", // Updated to match your gs:// link
   messagingSenderId: "745728416819",
   appId: "1:745728416819:web:da2dfb86cc5b79fb0d1746",
   measurementId: "G-FFHYQC4YJV"
