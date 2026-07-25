05669763c434b4bba39759cb8a3c98ab67ad823c
Kaled Al <kalalwayly@gmail.com>
2026-07-24 21:26:20 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,14 @@
-// KalMarket Firebase Functions
+/**
+ * Import function triggers from their respective submodules.
+ */
+
+const { setGlobalOptions } = require("firebase-functions");
+const { onRequest } = require("firebase-functions/https");
+const logger = require("firebase-functions/logger");
+
+setGlobalOptions({ maxInstances: 10 });
+
+exports.kalMarketTest = onRequest((request, response) => {
+  logger.info("KalMarket Functions are working!");
+  response.send("KalMarket Firebase Functions are alive!");
+});
